706b9702eb00508bd70a3ff84f50832bed0fab3c
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-07-18 18:21:12 +0300

    +Edit serverjs file for accepting body from request

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", goalRoutes);
 
