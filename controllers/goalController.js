92681ee99dc12907380e31d79a2d085bd81c7e89
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-07-19 11:11:32 +0300

    +ErrorHandler middleware initialize +Express-async-handler package to handling async functions

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,21 +1,28 @@
-export const getGoals = async (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-};
+import asyncHandler from "express-async-handler";
 
-export const getGoal = async (req, res) => {
+export const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+});
+
+export const getGoal = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: "Get Goal by Id", goalId: req.params.goalId });
-};
+});
 
-export const createGoal = async (req, res) => {
+export const createGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
   res.status(200).json({ message: "Create Goal" });
-};
+});
 
-export const updateGoal = async (req, res) => {
+export const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Update Goal by Id" });
-};
+});
 
-export const deleteGoal = async (req, res) => {
+export const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Delete a Goal by Id" });
-};
+});
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import errorHandler from "./middlewares/errorHandler.js";
 import goalRoutes from "./routes/goalRoutes.js";
-
 dotenv.config();
 const port = process.env.PORT || 3000;
 
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// app.use((err,req,res,next)=>{
+//     console.log("asd");
+// })
 app.use("/api", goalRoutes);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port :${port}`));
