94aec30587859d42057b15d44dd38fa3771f76cf
Mehmet Ali ÇELEBİ <mehmetalicelebii@gmail.com>
2023-07-20 13:48:01 +0300

    +DB(mongo) connection

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import errorHandler from "./middlewares/errorHandler.js";
 import goalRoutes from "./routes/goalRoutes.js";
-dotenv.config();
+import { connectDB } from "./config/db.js";
 const port = process.env.PORT || 3000;
+dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());
